31ce4d838ab4cf1d4610ff66f7a726a32d88b0bd
Avedis Boudakian <avedisb30@yahoo.com>
2024-02-06 17:36:19 -0500

    keyboard&mouse code

--- controller/control/control_operator_interface.cpp
+++ controller/control/control_operator_interface.cpp
@@ -32,43 +32,120 @@ ControlOperatorInterface::ControlOperatorInterface(Remote &remote) : remote(remo
 // STEP 2 (Tank Drive): Add getChassisTankLeftInput and getChassisTankRightInput function
 // definitions
 float ControlOperatorInterface::getChassisOmniLeftFrontInput() {
-    float horizontal = limitVal(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1.0f, 1.0f);
-    float vertical   = limitVal(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1.0f, 1.0f);
-    float rightHorizontal = limitVal(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1.0f, 1.0f);
-
-    float result = vertical + horizontal + rightHorizontal;
-
-    return result;
+    //float result = vertical + horizonttal + right horizontal
+    if  (remote.keyPressed(Remote::Key::W)){
+        float vertical   = limitVal((Remote::Key::W), 1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::A)){
+        float horizontal = limitVal((Remote::Key::A),-1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    } 
+    else if (remote.keyPressed(Remote::Key::S)){
+        float vertical   = limitVal((Remote::Key::S), -1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::D)){
+        float horizontal = limitVal((Remote::Key::D),1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    }
+    else{
+        float vertical = limitVal(0.0f, 1.0f,1.0f);
+        float result = vertical;
+        return result
+    }
 }
 
 float ControlOperatorInterface::getChassisOmniLeftBackInput() {
-    float horizontal  = limitVal(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1.0f, 1.0f);
-    float vertical    = limitVal(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1.0f, 1.0f);
-    float rightHorizontal = limitVal(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1.0f, 1.0f);
-
-    float result = vertical - horizontal + rightHorizontal;
-
-    return result;
+    //float result = vertical - horizontal + rightHorizontal;
+    if  (remote.keyPressed(Remote::Key::W)){
+        float vertical   = limitVal((Remote::Key::W), 1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::A)){
+        float horizontal = limitVal((Remote::Key::A),-1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    } 
+    else if (remote.keyPressed(Remote::Key::S)){
+        float vertical   = limitVal((Remote::Key::S), -1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::D)){
+        float horizontal = limitVal((Remote::Key::D),1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    }
+    else{
+        float vertical = limitVal(0.0f, 1.0f,1.0f);
+        float result = vertical;
+        return result
+    }
 }
 
 float ControlOperatorInterface::getChassisOmniRightFrontInput() {
-    float horizontal = limitVal(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1.0f, 1.0f);
-    float vertical   = limitVal(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1.0f, 1.0f);
-    float rightHorizontal = limitVal(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1.0f, 1.0f);
+    //float result = vertical - horizontal - rightHorizontal;
 
-    float result = vertical - horizontal - rightHorizontal;
-
-    return result;
+    if  (remote.keyPressed(Remote::Key::W)){
+        float vertical   = limitVal((Remote::Key::W), 1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::A)){
+        float horizontal = limitVal((Remote::Key::A),-1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    } 
+    else if (remote.keyPressed(Remote::Key::S)){
+        float vertical   = limitVal((Remote::Key::S), -1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::D)){
+        float horizontal = limitVal((Remote::Key::D),1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    }
+    else{
+        float vertical = limitVal(0.0f, 1.0f,1.0f);
+        float result = vertical;
+        return result
+    }
 }
 
 float ControlOperatorInterface::getChassisOmniRightBackInput() {
-    float horizontal = limitVal(remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1.0f, 1.0f);
-    float vertical   = limitVal(remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1.0f, 1.0f);
-    float rightHorizontal = limitVal(remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), -1.0f, 1.0f);
-
-    float result = vertical + horizontal - rightHorizontal;
-
-    return result;
+    //float result = vertical + horizontal - rightHorizontal;
+    if  (remote.keyPressed(Remote::Key::W)){
+        float vertical   = limitVal((Remote::Key::W), 1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::A)){
+        float horizontal = limitVal((Remote::Key::A),-1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    } 
+    else if (remote.keyPressed(Remote::Key::S)){
+        float vertical   = limitVal((Remote::Key::S), -1.0f, -1.0f, 1.0f);
+        float result = vertical;
+        return result;
+    }
+    else if (remote.keyPressed(Remote::Key::D)){
+        float horizontal = limitVal((Remote::Key::D),1.0f, -1.0f, 1.0f);
+        float result = horizontal;
+        return result;
+    }
+    else{
+        float vertical = limitVal(0.0f, 1.0f,1.0f);
+        float result = vertical;
+        return result
+    }
 }
 
-}  // namespace control
+}  // namespace control 
