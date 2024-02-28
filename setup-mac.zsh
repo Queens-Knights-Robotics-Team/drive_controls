fbd7d7dbfe78a7eaf6bd4988eb054d0a2c8be994
Hunter Coker <hunter.coker.2016@gmail.com>
2024-02-28 16:06:06 -0500

    verified and updated setup script for mac

--- setup-mac.zsh
+++ setup-mac.zsh
@@ -16,60 +16,64 @@ if ! command -v brew &> /dev/null; then
 else; echo "[INFO] 'homebrew' is already installed"
 fi
 
+echo "homebrew formulae:"
+
 # git
 if ! command -v git &> /dev/null; then
-    echo "[IMPORTANT] installing 'git' formula"
+    echo "  [IMPORTANT] installing 'git' formula"
     brew install git --quiet
-else; echo "[INFO] 'git' is already installed"
+else; echo "  [INFO] 'git' is already installed"
 fi
 
 # python3
 if ! command -v python3 &> /dev/null; then
-    echo "[IMPORTANT] installing 'python3' formula"
+    echo "  [IMPORTANT] installing 'python3' formula"
     brew install python3
-else; echo "[INFO] 'python3' is already installed"
+else; echo "  [INFO] 'python3' is already installed"
 fi
+
 
 # install ARM (target device) toolchain and tools
 brew tap osx-cross/arm
 #   arm-gcc-bin
 if ! brew ls --versions arm-gcc-bin &> /dev/null; then
-    echo "[IMPORTANT] installing 'arm-gcc-bin' formula"
+    echo "  [IMPORTANT] installing 'arm-gcc-bin' formula"
     brew install arm-gcc-bin --quiet
-else; echo "[INFO] 'arm-gcc-bin' is already installed"
+else; echo "  [INFO] 'arm-gcc-bin' is already installed"
 fi
 #   openocd
 if ! brew ls --versions openocd &> /dev/null; then
-    echo "[IMPORTANT] installing 'openocd --HEAD' formula"
+    echo "  [IMPORTANT] installing 'openocd --HEAD' formula"
     brew install openocd --HEAD --quiet
-else; echo "[INFO] 'openocd --HEAD' is already installed"
+else; echo "  [INFO] 'openocd --HEAD' is already installed"
 fi
 
 # install host gcc compiler and boost lib for tests
 #   boost
 if ! brew ls --versions boost &> /dev/null; then
-    echo "[IMPORTANT] installing 'boost' formula"
+    echo "  [IMPORTANT] installing 'boost' formula"
     brew install boost --quiet
-else; echo "[INFO] 'boost' is already installed"
+else; echo "  [INFO] 'boost' is already installed"
 fi
 #   gcc@11
 if ! brew ls --versions gcc@11 &> /dev/null; then
-    echo "[IMPORTANT] installing 'gcc@11' formula"
+    echo "  [IMPORTANT] installing 'gcc@11' formula"
     brew install gcc@11 --quiet
-else; echo "[INFO] 'gcc@11' is already installed"
+else; echo "  [INFO] 'gcc@11' is already installed"
 fi
 #   cmake
 if ! brew ls --versions cmake &> /dev/null; then
-    echo "[IMPORTANT] installing 'cmake' formula"
+    echo "  [IMPORTANT] installing 'cmake' formula"
     brew install cmake --quiet
-else; echo "[INFO] 'cmake' is already installed"
+else; echo "  [INFO] 'cmake' is already installed"
 fi
 
 # download and install 'clang-format-10'
+echo "clang-format-10:"
 if [ ! -d "${HOME}/.qkrt/clang+llvm-10.0.0-x86_64-apple-darwin" ]; then
-    echo "[IMPORTANT] downloading 'clang+llvm-10.0.0' binaries"
+    echo "  [IMPORTANT] downloading 'clang+llvm-10.0.0' binaries"
     curl -sL https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-apple-darwin.tar.xz | tar xz - -C ~/.qkrt
-else; echo "[INFO] 'clang+llvm-10.0.0' already downloaded"
+else; echo "  [INFO] 'clang+llvm-10.0.0' already downloaded"
 fi
 appended=false
 required="export PATH=\$HOME/.qkrt/clang+llvm-10.0.0-x86_64-apple-darwin/bin/:\$PATH"
@@ -80,10 +84,10 @@ while IFS= read -r line; do
     fi
 done < "${HOME}/.zshrc"
 if [ "${appended}" = false ]; then
-    echo "[IMPORTANT] adding 'clang+llvm-10.0.0' binaries to \$PATH"
+    echo "  [IMPORTANT] adding 'clang+llvm-10.0.0' binaries to \$PATH"
     echo -n "${required}\n" >> ~/.zshrc
     source ~/.zshrc
-else; echo "[INFO] 'clang+llvm-10.0.0' binaries already added to \$PATH"
+else; echo "  [INFO] 'clang+llvm-10.0.0' binaries already added to \$PATH"
 fi
 
 # if [ $(pip3 list | grep -c pipenv) -eq 0 ]; then
@@ -111,5 +115,9 @@ fi
 #     pip3 install Jinja2 --quiet
 # else; echo "\t[INFO] 'Jinja2' package already installed"
 # fi
+
+echo "error patches:"
+echo "  TODO: add 'setuptools' package to Piplock"
+echo "  TODO: change \"...\" to r\"...\" in: aruw-edu/taproot/modm/src/modm/platform/dma/stm32/module.lb"
 
 echo "\ndone."
